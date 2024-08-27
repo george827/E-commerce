b40a7f79ee3dd4ef42993ca351b4eaf522b32a73
george827 <kinyanjuigeorge827@gmail.com>
2024-08-27 15:58:54 +0300

    active nav

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useState } from "react";
 import "./Navbar.css";
 import Logo from "../assets/Frontend_Assets/logo.png";
 import cart_icon from "../assets/Frontend_Assets/cart_icon.png";
 
 export const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -11,14 +13,17 @@ export const Navbar = () => {
         <p>shopper</p>
       </div>
       <ul className="nav-menu">
-        <li>shop <hr/> </li>
-        <li>men</li>
-        <li>women</li>
-        <li>kids</li>
+        <li onClick={()=> {setMenu("shop")}}>
+          shop <hr />
+        </li>
+        <li onClick={()=> {setMenu("mens")}}>men</li>
+        <li onClick={()=> {setMenu("women")}}>women</li>
+        <li onClick={()=> {setMenu("kids")}}>kids</li>
       </ul>
       <div className="nav-login-cart">
         <button>login</button>
         <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   );
