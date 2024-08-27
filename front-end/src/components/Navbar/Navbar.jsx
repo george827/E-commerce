34fe747487330ec315afbec0fda62bbf558a8072
george827 <kinyanjuigeorge827@gmail.com>
2024-08-27 13:12:55 +0300

    Add nav

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import Logo from "../assets/Frontend_Assets/logo.png";
+import cart_icon from "../assets/Frontend_Assets/cart_icon.png";
 
 export const Navbar = () => {
   return (
-    <div className='NavBar'>
-        Nav bar
-        </div>
-  )
-}
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={Logo} alt="logo" />
+        <p>shopper</p>
+      </div>
+      <ul className="nav-menu">
+        <li>shop <hr/> </li>
+        <li>men</li>
+        <li>women</li>
+        <li>kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>login</button>
+        <img src={cart_icon} alt="" />
+      </div>
+    </div>
+  );
+};
