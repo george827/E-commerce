4a2b47c9af5b911cda45e39d12dac8f89df701fd
george827 <kinyanjuigeorge827@gmail.com>
2024-10-04 14:36:00 +0300

    Add style to hero

--- front-end/src/components/Hero/Hero.jsx
+++ front-end/src/components/Hero/Hero.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Hero.css'
+import handHandIcon from '../assets/Frontend_Assets/hand_icon.png'
+import arrowIcon from '../assets/Frontend_Assets/arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>Hero</div>
+    <div className='hero'>
+      <div className="hero-right">
+        <h2>New arrivals only</h2>
+        <div className="hand-hand-icon">
+          <p>new</p>
+          <img src={ handHandIcon} alt="hand" />
+        </div>
+        <p>collections</p>
+        <p>For everyone</p>
+      </div>
+      <div className="hero-latest-btn">
+        <div>latest collection</div>
+        <img src={arrowIcon} alt="arrow-left"/>
+      </div>
+      <div className="hero-left"></div>
+    </div>
   )
 }
 
