8504222b5549914b206f75f4daa855385e1217e8
george827 <kinyanjuigeorge827@gmail.com>
2024-10-07 23:04:32 +0300

    Add offers style

---
+++ front-end/src/components/NewCollection/NewCollection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './NewCollection.css'
+
+const NewCollection = () => {
+  return (
+    <div className='new-collection'>NewCollection</div>
+  )
+}
+
+export default NewCollection
