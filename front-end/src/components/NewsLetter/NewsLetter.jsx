d771efa12a3ed064939a92c04c3d6f2632f5ac3b
george827 <kinyanjuigeorge827@gmail.com>
2024-10-09 22:37:41 +0300

    Added news-letter

--- front-end/src/components/NewsLetter/NewsLetter.jsx
+++ front-end/src/components/NewsLetter/NewsLetter.jsx
@@ -3,11 +3,11 @@ import "./NewsLetter.css";
 
 const NewsLetter = () => {
   return (
-    <div className="news-letter">
+    <div className="newsletter">
       <h1>GET EXCLUSSIVE OFFER ON YOUR EMAIL</h1>
       <p>subscribe to our news letter and stay updated</p>
       <div>
-        <input type="email" placeholder="Your email id" />
+        <input type="email" placeholder="Your Email Id" />
         <button>Subscribe</button>
       </div>
     </div>
