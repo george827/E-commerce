6926ab87b5c7e4b6801aee7e1172b4c38e1dbf37
george827 <kinyanjuigeorge827@gmail.com>
2024-10-10 23:34:22 +0300

    Added socials

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -7,6 +7,19 @@ const Footer = () => {
     <div className='gooter'>
         <div className="footer-logo">
             <img src={logo} alt="logo" />
+            <p>Shopper</p>
+        </div>
+        <ul className="footer-links">
+            <li>company</li>
+            <li>Products</li>
+            <li>Offices</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <div className="footer-social-icon">
+            <div className="footer-icons-container">
+                
+            </div>
         </div>
         </div>
   )
