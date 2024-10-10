a4bb615f4138519015159b916212bd55aa055015
george827 <kinyanjuigeorge827@gmail.com>
2024-10-10 23:32:18 +0300

    Added Footer logo

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Footer.css'
+import logo from '../assets/Frontend_Assets/logo_big.png'
 
 const Footer = () => {
   return (
     <div className='gooter'>
-        
+        <div className="footer-logo">
+            <img src={logo} alt="logo" />
+        </div>
         </div>
   )
 }
