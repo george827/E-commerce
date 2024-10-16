02fff2e065afdbc001b89a1292d69fa609dfb29d
george827 <kinyanjuigeorge827@gmail.com>
2024-10-16 23:23:18 +0300

    context

--- front-end/src/context/ShopContext.jsx
+++ front-end/src/context/ShopContext.jsx
@@ -1,7 +1,16 @@
 import React, { createContext } from 'react'
+import AllProduct from '../components/assets/Frontend_Assets/all_product'
 
 export const ShopContext = createContext(null);
 
 const shopContextProvider = (props) => {
-    const contextValue ={};
+    const contextValue ={AllProduct};
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
 }
+
+export default shopContextProvider;
