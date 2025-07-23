a90db6767f62b779951768b392b575e434d4ccea
george827 <kinyanjuigeorge827@gmail.com>
2025-07-23 07:47:27 +0300

    Add context

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,9 @@ import { Product } from "./pages/Product";
 import { Cart } from "./pages/Cart";
 import { LoginSignup } from "./pages/LoginSignup";
 import Footer from "./components/Footer/Footer";
+import men_banner from "./components/assets/Frontend_Assets/banner_mens.png";
+import women_banner from "./components/assets/Frontend_Assets/banner_women.png";
+import kid_banner from "./components/assets/Frontend_Assets/banner_kids.png";
 
 function App() {
   return (
@@ -15,9 +18,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/men" element={<ShopCategory category="men" />} />
-          <Route path="/women" element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route path="/men" element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path="/women" element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kids" />} />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
--- front-end/src/pages/ShopCategory.jsx
+++ front-end/src/pages/ShopCategory.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../context/ShopContext';
 
-export const ShopCategory = () => {
+export const ShopCategory = (props) => {
+  const {all_products} = useContext(ShopContext);
   return (
-    <div>ShopCategory</div>
+    <div className='shop-category'>
+
+    </div>
   )
 }
