87588ab9c225af948b5c3ad0191fd74bd5b0b055
george827 <kinyanjuigeorge827@gmail.com>
2025-07-26 16:12:50 +0300

    set up login page

--- front-end/src/pages/Loginsignup.jsx
+++ front-end/src/pages/Loginsignup.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
   return (
@@ -9,6 +10,14 @@ export const LoginSignup = () => {
           <input type="text" placeholder='Your name' />
           <input type="email" placeholder='Email address' />
           <input type="password" placeholder='password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'> Already have an Account? 
+          <span>Logn In</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id='' />
+          <p>I agree to the <span>Terms of Use</span> and <span>Privacy Policy</span></p>
         </div>
       </div>
     </div>
