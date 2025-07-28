a40f385a57429c8d131e2c47e79b3f1802630c73
george827 <kinyanjuigeorge827@gmail.com>
2025-07-28 23:02:41 +0300

    Product

--- front-end/src/pages/Product.jsx
+++ front-end/src/pages/Product.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { ShopContext } from '../context/ShopContext';
+import { useParams } from 'react-router-dom';
 
 export const Product = () => {
+  const {all_products} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_products.find(item => item.id === parseInt(productId));
   return (
     <div>Product</div>
   )
