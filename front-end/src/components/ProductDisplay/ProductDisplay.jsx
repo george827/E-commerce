29f37370b970b91e5ad7767a3024e7736e94f529
george827 <kinyanjuigeorge827@gmail.com>
2025-07-30 00:56:40 +0300

    Update product UI

--- front-end/src/components/ProductDisplay/ProductDisplay.jsx
+++ front-end/src/components/ProductDisplay/ProductDisplay.jsx
@@ -1,11 +1,58 @@
 import React from 'react'
 import './ProductDisplay.css'
+import star_icon from "../assets/Frontend_Assets/star_icon.png";
+import star_dull_icon from "../assets/Frontend_Assets/star_dull_icon.png";
 
-export const ProductDisplay = () => {
+export const ProductDisplay = (props) => {
+  const {product} = props;
   return (
-    <div>
-        <div className="productdisplay-left"></div>
-        <div className="productdisplay-right"></div>
+    <div className='productdisplay-container'>
+        <div className="productdisplay-left">
+          <div className="display-img-list">
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+          </div>
+          <div className="product-disp-img">
+            <img className='productdisplay-main-img' src={product.image} alt="" />
+          </div>
+        </div>
+        <div className="productdisplay-right">
+          <h1>{product.name}</h1>
+          <div className="productdisplay-right-star">
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_dull_icon} alt="" />
+            <p>(122)</p>
+          </div>
+          <div className="productdisplay-rught-prices">
+            <div className="productdisplay-right-prices-old">
+              ${product.old_price}
+            </div>
+            <div className="productdisplay-right-prices-new">
+              ${product.new_price}
+            </div>
+          </div>
+          <div className="productdisplayright-description">
+            A short description of the product goes here. It should be concise and informative, highlighting the key features and benefits of the product.
+          </div>
+          <div className="productdisplay-right-size">
+            <h1>select size</h1>
+            <div className="productdisplay-right-sizes">
+              <div>S</div>
+              <div>M</div>
+              <div>L</div>
+              <div>XL</div>
+              <div>XXL</div>
+            </div>
+          </div>
+          <button>ADD TO CART</button>
+          <p className="productdisplay-right-category"> <span>Category :</span> Women, T-shirt, Crop Top</p>
+          <p className="productdisplay-right-category"> <span>Tags :</span> Modern, Latest</p>
+        </div>
     </div>
   )
 }
