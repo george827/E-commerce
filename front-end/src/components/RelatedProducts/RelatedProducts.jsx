785107f5348eaabbe43d01a40936c6ac2d0e61b1
george827 <kinyanjuigeorge827@gmail.com>
2025-07-31 23:53:41 +0300

    Add related UI

--- front-end/src/components/RelatedProducts/RelatedProducts.jsx
+++ front-end/src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import "./RelatedProducts.css"
+import data_products from '../assets/Frontend_Assets/data';
+import Item from '../Item/Item';
 
 export const RelatedProducts = () => {
   return (
-    <div>RelatedProducts</div>
+    <div className='relatedproducts'>
+        <h1>related products</h1>
+        <hr />
+        <div className="relatedproducts-item">
+            {data_products.map((item, i) => {
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            })}
+        </div>
+        </div>
   )
 }
