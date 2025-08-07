b6f2d75d00e875a0c4935521fdddd09e0a7f09af
george827 <kinyanjuigeorge827@gmail.com>
2025-08-08 00:10:55 +0300

    Add Cart remove and Add func

--- front-end/src/context/ShopContext.jsx
+++ front-end/src/context/ShopContext.jsx
@@ -3,16 +3,34 @@ import all_products from '../components/assets/Frontend_Assets/all_product'
 
 export const ShopContext = createContext(null);
 
-const ShopContextProvider = (props) => {
-    const contextValue ={all_products};
-
-    const getDefaultCart = () => {
+const getDefaultCart = () => {
         let cart = {};
         for (let i = 0; i < all_products.length + 1; i++) {
             cart[i] = 0; // Initialize each product's quantity to 0
         }
         return cart;
     }
+
+const ShopContextProvider = (props) => {
+
+    const [cartItem, setCartItem] = React.useState(getDefaultCart());
+
+    const addToCart = (itemId) => {
+        setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItem((prev) => {
+            const newCart = { ...prev };
+            if (newCart[itemId] > 0) {
+                newCart[itemId] -= 1;
+            }
+            return newCart;
+        }
+        );
+    }
+
+    const contextValue ={all_products, cartItem, addToCart, removeFromCart};    
 
     return (
         <ShopContext.Provider value={contextValue}>
