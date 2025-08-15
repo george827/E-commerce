385ce4fb6cac7a16c3d64b2347fc25ec329df052
george827 <kinyanjuigeorge827@gmail.com>
2025-08-15 22:20:45 +0300

    Update backend

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 // image storage engine
 const storage = multer.diskStorage({
-    destination: 'uploads/images',
+    destination: 'upload/images',
     filename: (req, file, cb) => {
         cb(null, `${file.fieldname}_${Date.now()}_${path.extname(file.originalname)}`);
     }
@@ -32,13 +32,64 @@ const upload = multer({ storage: storage });
 
 // creating upload endpoint for images
 
-app.use('/images', express.static('uploads/images'));
+app.use('/images', express.static('upload/images'));
 
 app.post('/upload', upload.single('product'), (req, res) => {
     res.json({
         success: 1,
         image_url: `http://localhost:${port}/images/${req.file.filename}`
     });
+});
+
+// schema for creating product
+const Product = mongoose.model("Product",{
+    id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    new_price: {
+        type: Number,
+        required: true
+    },
+    old_price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    available: {
+        type: Boolean,
+        default: true
+    },
+    
+})
+
+app.post('/addproduct', async (req, res) => {
+    const { id, name, image, category, new_price, old_price } = req.body;
+    try {
+        const product = new Product({ id, name, image, category, new_price, old_price });
+        console.log("Product data:", product);
+        await product.save();
+        console.log("Product added successfully");
+        res.json({ success: true, name:req.body.name, message: "Product added successfully" });
+    } catch (error) {
+        console.error("Error adding product:", error);
+        res.status(500).json({ success: 0, message: "Failed to add product" });
+    }
 });
 
 app.listen(port, (err) => {
